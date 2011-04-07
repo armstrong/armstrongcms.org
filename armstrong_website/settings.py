e9234533514b9c320743cf9c91b9825caad511d5
Aurelio D. Tinio <atinio@baycitizen.org>
2011-04-06 17:19:46 -0700

    add django-chunks

--- armstrong_website/settings.py
+++ armstrong_website/settings.py
@@ -126,6 +126,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    
+    'chunks',
 )
 
 # A sample logging configuration. The only tangible logging
