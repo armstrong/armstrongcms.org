840c6288cb23dcc755b119456486988c9454fa90
Aurelio D. Tinio <atinio@baycitizen.org>
2011-04-07 18:27:12 -0700

    add well templates for django-chunks

--- armstrong_website/home/views.py
+++ armstrong_website/home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from armstrong.core.arm_wells.models import Well
+
 def index(request, template_name='home/index.html'):
     context = {}
+    wells = Well.objects.filter(type__slug='generic')
+    context['well'] = wells[0] if wells else None 
     return render_to_response(template_name, context, 
                               context_instance=RequestContext(request))
