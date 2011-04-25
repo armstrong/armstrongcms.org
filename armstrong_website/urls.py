55343225c938e2b4c22583e661b2af0b741a576c
Aurelio D. Tinio <atinio@baycitizen.org>
2011-04-25 14:29:09 -0700

    use arm_wells generic SimpleWellView

--- armstrong_website/urls.py
+++ armstrong_website/urls.py
@@ -4,9 +4,12 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from armstrong.core.arm_wells.views import SimpleWellView
+
 urlpatterns = patterns('',
     url(r'^contact/', include('contact_form.urls')),
-    url(r'^$', 'home.views.index', name='home'),
+    url(r'^$', SimpleWellView.as_view(template_name='home/index.html',
+                                      well_title='Generic'), name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
