c7332bc448e421176121d163c5ca361523ac9a96
Travis Swicegood <development@domain51.com>
2011-11-07 15:13:07 -0600

    the basics our of contact form

---
+++ armstrong_website/arm_contact_forms/forms.py
@@ -0,0 +1,29 @@
+from contact_form.forms import ContactForm
+
+
+class ArmstrongContactForm(ContactForm):
+    company = forms.CharField(max_length=100,
+                              widget=forms.TextInput(attrs=attrs_dict),
+                              label=u'Company')
+    from_email = 'info@armstrongcms.org'
+    subject_template_name = "contact_form/contact_form_subject_armstrong.txt"
+    template_name = 'contact_form/contact_form_armstrong.txt'
+
+    @property
+    def recipient_list(self):
+        return [self.cleaned_data["email"], ]
+
+    def save(self, fail_silently=False):
+        smtp_connection = get_connection('django.core.mail.backends.smtp.EmailBackend',
+                                         host=settings.SOCKETLABS_HOST,
+                                         username=settings.SOCKETLABS_USER,
+                                         password=settings.SOCKETLABS_PASSWORD)
+
+        from django.core.mail import EmailMessage
+        message_dict = self.get_message_dict()
+        message_dict["to"] = message_dict.pop("recipient_list")
+        message_dict["cc"] = [message_dict["from_email"], ]
+        message_dict["body"] = message_dict.pop("message")
+        message = EmailMessage(connection=smtp_connection,
+                bcc=[settings.BCC_EMAIL,], **message_dict)
+        message.send(fail_silently=fail_silently)
